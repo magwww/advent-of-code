9240529a93c84e27c0de3dde4a9defc16a1e8c5f
Magda <wozna.magda@gmail.com>
2024-09-19 13:33:34 +0200

    Add 2023/2 part 2

--- 2023/2/index.ts
+++ 2023/2/index.ts
@@ -1,5 +1,7 @@
 import { data, comparisonObject } from "./data";
 
+//PART 1
+
 //compare objects and see if any value exceeds ones in comparisonObject
 const compareObject = (
   obj: Record<string, number>,
@@ -33,3 +35,35 @@ const sum = findValidIndexes(data, comparisonObject).reduce(
 );
 
 console.log("sum", sum);
+
+//PART 2
+
+type CubesColors = "red" | "blue" | "green";
+
+const findMinimumSetOfCubes = (
+  arr: Array<Array<Record<string, number>>>
+): Array<Array<number>> => {
+  return arr.map((subArray) => {
+    const maxValues = { green: 0, blue: 0, red: 0 };
+
+    subArray.forEach((obj) => {
+      for (let key in obj) {
+        if (obj[key] > maxValues[key as CubesColors]) {
+          maxValues[key as CubesColors] = obj[key];
+        }
+      }
+    });
+
+    return Object.values(maxValues);
+  });
+};
+
+const leastNumberOfCubes = findMinimumSetOfCubes(data);
+
+const power = leastNumberOfCubes.map((subArray) =>
+  subArray.reduce((acc, num) => acc * num, 1)
+);
+
+const sumOfPowers = power.reduce((acc, value) => acc + value, 0);
+
+console.log("sumOfPowers", sumOfPowers);
